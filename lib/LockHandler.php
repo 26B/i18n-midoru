6b9bd2c943d66f399dc043314f0ba610670a41df
Goncalo Simoes <goncalo.simoes@26b.io>
2021-03-05 18:10:31 +0000

    Documentation

--- lib/LockHandler.php
+++ lib/LockHandler.php
@@ -6,21 +6,61 @@ use ArrayObject;
 
 /**
  * Handles the lock file, reading and writing.
+ *
+ * @since      0.0.0
+ * @package    TODO:
+ * @subpackage TODO:
+ * @author     TODO:
  */
 class LockHandler extends ArrayObject {
 
+	/**
+	 * Singleton instance.
+	 *
+	 * @since 0.0.0
+	 * @var   null|LockHandler
+	 */
 	private static $instance = null;
 
+	/**
+	 * Lock file name.
+	 *
+	 * @since 0.0.0
+	 * @var   string
+	 */
 	const FILENAME = 'midoru.lock';
 
+	/**
+	 * File path to lock.
+	 *
+	 * @since 0.0.0
+	 * @var   string
+	 */
 	private $path = '';
 
+	/**
+	 * MD5 of the data read on the first instance. Used for dirty state handling.
+	 *
+	 * @since 0.0.0
+	 * @var   string
+	 */
 	private $md5 = '';
 
-	public static function get_instance() {
+	/**
+	 * Get the singleton instance or create one if it doesn't exist.
+	 *
+	 * @since  0.0.0
+	 * @return LockHandler
+	 */
+	public static function get_instance() : LockHandler {
 		return self::$instance === null ? self::$instance = new LockHandler() : self::$instance;
 	}
 
+	/**
+	 * Read the data from lock file if it exists.
+	 *
+	 * @since 0.0.0
+	 */
 	private function __construct() {
 		$this->path = getcwd() . '/' . self::FILENAME;
 		$file       = fopen( $this->path, 'r' );
@@ -37,18 +77,28 @@ class LockHandler extends ArrayObject {
 		parent::__construct( $data );
 	}
 
-	public function write() {
+	/**
+	 * Write the currently loaded data to the lock file, if there are any changes.
+	 *
+	 * @since  0.0.0
+	 * @return void
+	 * @throws Exception When file open fails.
+	 */
+	public function write() : void {
 		$data = $this->getArrayCopy();
 
 		// Avoid unnecessary writes.
-		if ( md5( json_encode( $data ) ) === $this->md5 ) {
+		$new_md5 = md5( json_encode( $data ) );
+		if ( $new_md5 === $this->md5 ) {
 			return;
 		}
 
 		$file = fopen( $this->path, 'w' );
 		if ( ! $file ) {
-			throw new \Exception( 'Failure to write lock file.' );
+			throw new \Exception( 'Failure to open lock file for writing.' );
 		}
+
+		$this->md5 = $new_md5;
 
 		// TODO: Should we save the file its empty?
 		fwrite( $file, empty( $data ) ? '{}' : json_encode( $data, JSON_PRETTY_PRINT ) );
