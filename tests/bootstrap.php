331c42455bf36f253f11723e24ba22df8910909e
Goncalo Simoes <goncalo.simoes@26b.io>
2021-03-12 11:26:28 +0000

    Command bin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,3 @@
 
 // First we need to load the composer autoloader so we can use WP Mock
 require_once './vendor/autoload.php';
-
-// WordPress specifics activate below
-
-// WP_Mock::activateStrictMode();
-// WP_Mock::bootstrap();
