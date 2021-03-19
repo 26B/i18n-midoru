027af0d58d2d2bddc1b9f05f463017cfef9e7fb8
Goncalo Simoes <goncalo.simoes@26b.io>
2021-03-19 16:56:21 +0000

    Add documentation to commands

--- lib/Console/DownloadCommand.php
+++ lib/Console/DownloadCommand.php
@@ -6,15 +6,43 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Command for downloading translations.
+ *
+ * @since      0.0.0
+ * @package    TODO:
+ * @subpackage TODO:
+ * @author     TODO:
+ */
 class DownloadCommand extends Command {
 
+	/**
+	 * Command name.
+	 *
+	 * @since 0.0.0
+	 * @var   string
+	 */
 	protected static $defaultName = 'download';
 
+	/**
+	 * Configure command.
+	 *
+	 * @since  0.0.0
+	 * @return void
+	 */
 	protected function configure() {
 		$this->setDescription( 'Download translations.' )
 			->setHelp( 'Downloads translations according to the configuration in i18n-midoru.json.' );
 	}
 
+	/**
+	 * Execute command.
+	 *
+	 * @since  0.0.0
+	 * @param  InputInterface  $input
+	 * @param  OutputInterface $output
+	 * @return int
+	 */
 	protected function execute( InputInterface $input, OutputInterface $output ) {
 		$t = new \TwentySixB\Translations\Translations();
 		$t->download();
--- lib/Console/MakePotsCommand.php
+++ lib/Console/MakePotsCommand.php
@@ -6,15 +6,43 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Command for making pot files.
+ *
+ * @since      0.0.0
+ * @package    TODO:
+ * @subpackage TODO:
+ * @author     TODO:
+ */
 class MakePotsCommand extends Command {
 
+	/**
+	 * Command name.
+	 *
+	 * @since 0.0.0
+	 * @var   string
+	 */
 	protected static $defaultName = 'make_pots';
 
+	/**
+	 * Configure command.
+	 *
+	 * @since  0.0.0
+	 * @return void
+	 */
 	protected function configure() {
 		$this->setDescription( 'Make pot files for translations.' )
 			->setHelp( 'Make pot files for translations according to the configuration in i18n-midoru.json.' );
 	}
 
+	/**
+	 * Execute command.
+	 *
+	 * @since  0.0.0
+	 * @param  InputInterface  $input
+	 * @param  OutputInterface $output
+	 * @return int
+	 */
 	protected function execute( InputInterface $input, OutputInterface $output ) {
 		$t = new \TwentySixB\Translations\Translations();
 		$t->make_pots();
